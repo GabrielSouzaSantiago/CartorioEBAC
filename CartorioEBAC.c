9671cdc7e67bcd0777de65182e3b0a48e502a5b0
GabrielSouzaSantiago <agendagabrielss@gmail.com>
2024-07-10 19:20:51 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- CartorioEBAC.c
+++ CartorioEBAC.c
@@ -131,7 +131,8 @@ int main() // local de início "int" referente a numeros inteiros
     	printf("Escolha a opção desejada do menu:\n\n");
     	printf("\t1 - Registrar nomes\n");
     	printf("\t2 - Consultar nomes\n");
-    	printf("\t3 - Deletar nomes\n\n\n"); 
+    	printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sitema \n\n"); 
     	printf("Opção: "); //Fim do menu
      	
         scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -151,6 +152,11 @@ int main() // local de início "int" referente a numeros inteiros
 		   case 3:
 		   deletar(); //Chamada de funções
 		   break; //Forçar a saída de um laço de repetição "Quebra"
+		   
+		   case 4:
+		   printf("Obrigado por utilizar o sistema!\n");
+		   return 0;
+		   break;
 		   
 		   default:
 		   printf("Essa opção nao está disponivel!\n");
